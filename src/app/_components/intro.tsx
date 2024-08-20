95543e60c1745d6657e07c5ed94307d79fb99f8e
Conner Charlebois <conner.charlebois@nc6jrk6wwd.lan>
2024-08-20 09:45:07 -0400

    init

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,21 +1,10 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col flex md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Conner Charlebois
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
-      </h4>
+      <p>Hi! I'm Conner and I help people build delightful, robust web and mobile apps. Here you can find some tools and small helpers that I've created. You can also check me out on Medium or, follow me on Twitter</p>
     </section>
   );
 }
