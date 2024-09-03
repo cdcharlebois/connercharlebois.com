5f196d7e541b868b18ea38627c42482a0680d526
cdcharlebois <cdcharlebois@gmail.com>
2024-09-03 16:18:52 -0400

    add images to remember post

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,24 @@
 import { remark } from "remark";
 import html from "remark-html";
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+import remarkRehype from "remark-rehype";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  // console.log({markdown})
+  // const result = await remark().use(remarkParse).use(rehypeRaw).use(rehypeStringify).process(markdown);
+  const result = await unified().use(remarkParse)
+  .use(remarkRehype, {allowDangerousHtml: true})
+  .use(rehypeRaw)
+  .use(rehypeDocument)
+  .use(rehypeFormat)
+  .use(rehypeStringify)
+  .process(markdown);
+  // console.log({markdown, result})
   return result.toString();
 }
